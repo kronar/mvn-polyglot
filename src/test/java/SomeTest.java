ebced70909d3175dfef587f0f014f462c51c0f4d
nikita.makarov <nikita.makarov@odnoklassniki.ru>
2016-04-26 11:11:01 +0300

    Init

---
+++ src/test/java/SomeTest.java
@@ -0,0 +1,13 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by nikita on 26.04.16.
+ */
+public class SomeTest {
+
+    @Test
+    public void test(){
+        Assert.assertEquals(1,1);
+    }
+}
